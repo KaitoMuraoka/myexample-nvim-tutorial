dc4115a18020045c1bf5223ea6b68009420fe7f3
KaitoMuraoka <kaitoboshi@icloud.com>
2024-12-20 18:41:17 +0900

    設定ファイルで文字列を変更する

--- lua/myexample.lua
+++ lua/myexample.lua
@@ -1,1 +1,12 @@
-print("Hello Neovim")
+local M = {} -- モジュール名を宣言
+
+M.config = {
+	message = "Hello World",
+}
+
+M.setup = function(args)
+	M.config = vim.tbl_deep_extend("force", M.config, args or {}) -- plugin マネージャから引き受けた config の項目を M.config に結合
+	print(M.config.message) --print で結果を表示する
+end
+
+return M
